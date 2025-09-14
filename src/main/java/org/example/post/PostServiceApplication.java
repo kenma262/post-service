431dcca56d39e3078707a4d0becda7351b7581dc
kenma262 <kenma262@gmail.com>
2025-09-14 23:17:47 +0800

    add jenkins file

--- src/main/java/org/example/post/PostServiceApplication.java
+++ src/main/java/org/example/post/PostServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PostServiceApplication {
+    // test commit
     public static void main(String[] args) {
         SpringApplication.run(PostServiceApplication.class, args);
     }
